aa6cf6267d6bc489dbf553b703def50e7d9e9c41
tyson <tyson@trillionlab.io>
2022-06-15 16:34:00 +0800

    update test react

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+import GoogleAnalytics from "react-ga";
+
+
+//GoogleAnalytics
+GoogleAnalytics.initialize("UA-232132588-1");
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
