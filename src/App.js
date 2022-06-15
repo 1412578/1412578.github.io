0adb559ca8f4dce2dcde9f435281af701589bff9
tyson <tyson@trillionlab.io>
2022-06-15 17:28:52 +0800

    update test react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./styles.css";
+import GoogleAnalytics from "react-ga";
+import GoogleAnalytics4 from "react-ga4";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+    const [value, setValue] = useState();
+    return (
+        <div className="App">
+            <h1>Hello CodeSandbox</h1>
+            <h2>Start editing to see some magic happen!</h2>
+            <input type='text' onChange={e => setValue(e.target.value)} defaultValue="home"/>
+            <button onClick={() => {
+                GoogleAnalytics.pageview(value);
+                GoogleAnalytics4.send("pageview", value);
+            }}>click
+            </button>
+        </div>
+    );
 }
