cc805da057e206a8bdd88d467e53797bb9eab9d9
tyson <tyson@trillionlab.io>
2022-06-15 16:39:41 +0800

    update test react

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import GoogleAnalytics from "react-ga";
 //GoogleAnalytics
 GoogleAnalytics.initialize("UA-232132588-1");
 
+GoogleAnalytics.pageview("/home");
+
 root.render(
   <StrictMode>
     <App />
