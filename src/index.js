b25726fe2a6847b7fbd8d3adb62613583f6eb21f
tyson <tyson@trillionlab.io>
2022-06-15 16:50:51 +0800

    update test react

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 import GoogleAnalytics from "react-ga";
+import GoogleAnalytics4 from "react-ga4";
 
 
 //GoogleAnalytics
 GoogleAnalytics.initialize("UA-232132588-1");
+GoogleAnalytics4.initialize("G-RFMRQB5BY4");
 
 GoogleAnalytics.pageview("/home");
+GoogleAnalytics4.send("pageview");
+
 
 root.render(
   <StrictMode>
