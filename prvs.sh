0b6755c18d7ee581ab05b75eef7ce4c8c539abbb
Trần Quang Trí <32097182+1412578@users.noreply.github.com>
2024-10-27 10:40:04 +0700

    Update prvs.sh

--- prvs.sh
+++ prvs.sh
@@ -128,7 +128,7 @@ function provisioning_start() {
     when-changed "${WORKSPACE}/stable-diffusion-webui/config.json" rclone copy "${WORKSPACE}/stable-diffusion-webui/config.json"  "gdrivevastai:vastai" --config  "/opt/stable-diffusion-webui/reclone-config.json" &
  #   when-changed -r "${WORKSPACE}/stable-diffusion-webui/outputs" rclone copy "${WORKSPACE}/stable-diffusion-webui/outputs"  "gdrivevastai:vastai/outputs" --config  "/opt/stable-diffusion-webui/reclone-config.json" &
     when-changed "${WORKSPACE}/stable-diffusion-webui/extensions/stable-diffusion-webui-images-browser/wib.sqlite3" rclone copy "${WORKSPACE}/stable-diffusion-webui/extensions/stable-diffusion-webui-images-browser/wib.sqlite3"  "gdrivevastai:vastai/extensions/stable-diffusion-webui-images-browser" --config  "/opt/stable-diffusion-webui/reclone-config.json" &
-    when-changed -r "${WORKSPACE}/stable-diffusion-webui/log/images" rclone copy "${WORKSPACE}/stable-diffusion-webui/log/images"  "gdrivevastai:vastai/log/images" --config  "/opt/stable-diffusion-webui/reclone-config.json" &
+    when-changed -r "${WORKSPACE}/stable-diffusion-webui/log/images" rclone copy "${WORKSPACE}/stable-diffusion-webui/log/images"  "gdrivevastai:vastai/log/images" --config  "/opt/stable-diffusion-webui/reclone-config.json" --include "*.png"  &
  #   when-changed -r "${WORKSPACE}/stable-diffusion-webui/log" rclone copy "${WORKSPACE}/stable-diffusion-webui/log"  "gdrivevastai:vastai/log" --config  "/opt/stable-diffusion-webui/reclone-config.json" &
     
 }
